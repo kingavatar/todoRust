3f0dfb3c4a320f98f53e64c0d0299f812de8a9f8
kingavatar <saikiranreddy06@gmail.com>
2020-11-05 03:17:18 +0530

    Add Unit tests and Integration tests

--- src/main.rs
+++ src/main.rs
@@ -16,29 +16,8 @@ fn main() {
             _ => {}
         }
     }
-    let mut events: Vec<&str> = Vec::new();
-    let mut data = [0u8; 1512];
-    match TcpStream::connect("localhost:9977") {
-        Ok(mut stream) => {
-            let msg = b"events";
-            stream.write_all(msg).unwrap();
-            match stream.read(&mut data) {
-                Ok(n) => {
-                    if n == 0 {
-                        std::process::exit(0x0000);
-                    }
-                    let mut reply: Vec<&str> = from_utf8(&data[0..n]).unwrap().lines().collect();
-                    events.append(&mut reply);
-                }
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
-            }
-        }
-        Err(e) => {
-            println!("Failed to connect:{}", e);
-        }
-    }
+    let mut events: Vec<String> = Vec::new();
+    get_events(&mut events);
     for event in events.iter() {
         let mut splitter = event.splitn(2, ',');
         let first = splitter.next().unwrap();
@@ -49,7 +28,37 @@ fn main() {
             .datetime_from_str(second.trim(), "%d %B, %I:%M %p %Y")
             .unwrap();
         let duration = date - now;
-        if duration.num_days() >= 2 {
+        print_duration(duration, term, conky, first);
+    }
+}
+
+fn get_events(events:&mut Vec<String>){
+    let mut data = [0u8; 1512];
+    match TcpStream::connect("localhost:9977") {
+        Ok(mut stream) => {
+            let msg = b"events";
+            stream.write_all(msg).unwrap();
+            match stream.read(&mut data) {
+                Ok(n) => {
+                    if n == 0 {
+                        std::process::exit(0x0000);
+                    }
+                    let reply:Vec<String> = from_utf8(&data[0..n]).unwrap().lines().map(String::from).collect();
+                    events.extend(reply);
+                }
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Failed to connect:{}", e);
+        }
+    }
+}
+
+fn print_duration(duration:chrono::Duration,term:bool,conky:bool,first: &str){
+    if duration.num_days() >= 2 {
             if term {
                 println!(
                 "\x1b[38;2;250;169;22m{},\x1b[0m remaining days to submit are \x1b[38;2;104;161;223m{} days\x1b[0m",
@@ -82,4 +91,48 @@ fn main() {
             }
         }
     }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_get_events() {
+
+        let mut events: Vec<String> = Vec::new();
+        assert_eq!(get_events(&mut events),());
+    }
+    #[test]
+    fn test_not_connect(){
+        assert!(true); // should implement latter
+    }
+    #[test]
+    fn test_main(){
+        assert_eq!(main(),());
+        let duration:chrono::Duration = chrono::Duration::days(1);
+        assert_eq!(print_duration(duration, false, false, "first"),());
+    }
+
+    #[test]
+    fn test_term() {
+        let duration:chrono::Duration = chrono::Duration::days(2);
+        let term = true;
+        let conky = false;
+        let first:&str = "Testing ";
+        assert_eq!(print_duration(duration, term, conky, first),());
+        let duration:chrono::Duration = chrono::Duration::days(1);
+        assert_eq!(print_duration(duration, term, conky, first),());
+        
+    }
+
+    #[test]
+    fn test_conky() {
+        let duration:chrono::Duration = chrono::Duration::days(2);
+        let term = false;
+        let conky = true;
+        let first:&str = "Testing ";
+        assert_eq!(print_duration(duration, term, conky, first),());
+        let duration:chrono::Duration = chrono::Duration::days(1);
+        assert_eq!(print_duration(duration, term, conky, first),());
+    }
+
 }
---
+++ tests/cli.rs
@@ -0,0 +1,27 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use std::process::Command; // Run programs
+
+#[test]
+fn term_test() -> Result<(), Box<dyn std::error::Error>>{
+    let mut cmd = Command::cargo_bin("todo")?;
+    cmd.arg("term");
+    cmd.assert().success();
+    Ok(())
+}
+
+#[test]
+fn terminal_test() -> Result<(), Box<dyn std::error::Error>>{
+    let mut cmd = Command::cargo_bin("todo")?;
+    cmd.arg("terminal");
+    cmd.assert().success();
+    Ok(())
+}
+
+
+#[test]
+fn conky_test() -> Result<(), Box<dyn std::error::Error>>{
+    let mut cmd = Command::cargo_bin("todo")?;
+    cmd.arg("conky");
+    cmd.assert().success();
+    Ok(())
+}
